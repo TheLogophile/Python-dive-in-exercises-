bdd1451a446c00b4d8bd4c7bcb21bf8cd3d1b3f8
TheLogophile <fatemehakbari.16fa@gmail.com>
2025-08-12 18:01:31 +0330

    Range arguments edited

--- E01.py
+++ E01.py
@@ -6,7 +6,7 @@ def numgen(_):
     prefix = ["90", "91", "92", "93", "94", "96", "10", "19", "11", "12", "13", "14", "15", "16", "17", "18", "30",
               "33", "35", "36", "37", "38", "39", "01", "02", "03", "05"]
     numberl = ["09", random.choice(prefix)]
-    numberl.append(''.join(map(str,random.choices(range(0,9),k=7))))
+    numberl.append(''.join(map(str,random.choices(range(0,10),k=7))))
     return ''.join(numberl)
 
 numbers_list = list(map(numgen , range(100)))
