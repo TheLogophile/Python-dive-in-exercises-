1a8afe0b5a02b00b0137e1c86391944e6abf04ce
TheLogophile <fatemehakbari.16fa@gmail.com>
2025-08-12 17:58:35 +0330

    Replaced for loops with map(), choice() and choices

--- E01.py
+++ E01.py
@@ -5,11 +5,8 @@ import random
 def numgen(_):
     prefix = ["90", "91", "92", "93", "94", "96", "10", "19", "11", "12", "13", "14", "15", "16", "17", "18", "30",
               "33", "35", "36", "37", "38", "39", "01", "02", "03", "05"]
-
-    p = random.randint(0, len(prefix)-1)
-    numberl = ["09", prefix[p]]
-    for i in range(7):
-        numberl.append(str(random.randint(0,9)))
+    numberl = ["09", random.choice(prefix)]
+    numberl.append(''.join(map(str,random.choices(range(0,9),k=7))))
     return ''.join(numberl)
 
 numbers_list = list(map(numgen , range(100)))
